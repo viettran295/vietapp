3145231ba2d1ad61cdbb356be5842f3d86dac2a9
viet.tran <viettran295@gmail.com>
2022-08-04 20:24:15 +0200

    css works in static folder (flask)

--- dev/app.py
+++ dev/app.py
@@ -3,14 +3,18 @@ import datetime
 
 app = Flask(__name__)
 
-@app.route("/employments")
-def employments():
-    return render_template("employments.html")
-
 @app.route("/")
 def about():
     return render_template("about.html")
 
+@app.route("/employments")
+def employments():
+    return render_template("employments.html")
+
 @app.route("/certificate")
 def certificate():
     return render_template("certificate.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
