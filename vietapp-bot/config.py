b32104e062fcee6ae9b4e06c04427a9c1478eab4
viet.tran <viettran295@gmail.com>
2022-08-27 22:29:36 +0200

    Revert "Delete config.py"
    This reverts commit 109034d2b825e51c8acac04ac32c2b844b81caf5.

---
+++ vietapp-bot/config.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import os
+
+class DefaultConfig:
+    """ Bot Configuration """
+
+    PORT = 3978
+    APP_ID = os.environ.get("MicrosoftAppId", "")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    QNA_KNOWLEDGEBASE_ID = os.environ.get("QnAKnowledgebaseId", "63358840-6bd8-41f2-9139-ff6f23b0cbe3")
+    QNA_ENDPOINT_KEY = os.environ.get("QnAEndpointKey", "ba6d0e3c-d6c4-40ee-80bc-6fe55b942393")
+    QNA_ENDPOINT_HOST = os.environ.get("QnAEndpointHostName", "https://qna-bot-vietapp.azurewebsites.net/qnamaker")
+
+    LUIS_APP_ID = os.environ.get("LuisAppId", "99869189-930d-4807-b8f6-dd2979cfe130")
+    LUIS_API_KEY = os.environ.get("LuisAPIKey", "7209b1bba256424482b3d9104b39c93c")
+    # LUIS endpoint host name, ie "westus.api.cognitive.microsoft.com"
+    LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName", "https://bot-vietapp.cognitiveservices.azure.com/")
+
+class TranslatorConfig:
+        TRANSLATOR_KEY = "d07c486a86e04ccf90651d02c1b49ac1"
+        # Endpoint for text
+        TRANSLATOR_ENDPOINT = "https://api.cognitive.microsofttranslator.com"
+        TRANSLATOR_LOCATION = "westeurope"
+        TRANSLATE_PATH = "/translate"
+        TRANSLATE_CONSTRUCTED_URL = TRANSLATOR_ENDPOINT + TRANSLATE_PATH
