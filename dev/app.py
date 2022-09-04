d3b3c94d23c6101e80e774d3902e8970f8cbeb8a
viet.tran <viettran295@gmail.com>
2022-09-04 21:56:59 +0200

    test speech with mic

--- dev/app.py
+++ dev/app.py
@@ -1,12 +1,12 @@
-from flask import Flask, abort, render_template, request, flash, make_response, send_from_directory, current_app
+from flask import Flask, abort, render_template, request, send_from_directory
 import os
-# from speech_translator.speech_translator import Speech
-# from config import SpeechConfig
+from speech_translator.speech_translator import Speech
+from config import SpeechConfig
 
 app = Flask(__name__)
 
-# speech_config = SpeechConfig()
-# speech = Speech(speech_config)
+speech_config = SpeechConfig()
+speech = Speech(speech_config)
 
 @app.route("/")
 def about():
@@ -30,25 +30,18 @@ def bot_architekture():
 
 @app.route("/speech_translator", methods=['POST','GET'])
 def speech_translator():
-    return render_template("speech_translator.html")
-
-@app.route('/upload', methods=['POST'])
-def upload():
     if request.method == "POST":
-        if 'audio_file' in request.files:
-            file = request.files['audio_file']
-            # while True:
-            #     dst = os.path.join(current_app.instance_path,
-            #                         current_app.config.get('UPLOAD_FOLDER','upload'))
-            #     if not os.path.exists(dst):
-            #         break
-            file.save(file.filename)
-        return make_response('', 200)
+        # result = speech.recognize_from_microphone()
+        result = "This function will be updated soon"
+        return render_template("speech_translator.html", result=result)
+    else:
+        return render_template("speech_translator.html")
         
-@app.route("/static/<path:path>")
-def send_static(path):
-    return send_from_directory('static', path)
-# if __name__=="__main__":
-#     app.secret_key = 'super secret key'
-#     app.config['SESSION_TYPE'] = 'filesystem'
-#     app.run(debug=True,host='0.0.0.0')
+# @app.route("/static/<path:path>")
+# def send_static(path):
+#     return send_from_directory('static', path)
+
+if __name__=="__main__":
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
+    app.run(debug=True,host='0.0.0.0')
--- dev/speech_translator/speech_translator.py
+++ dev/speech_translator/speech_translator.py
@@ -8,8 +8,6 @@ class Speech:
 
     def recognize_from_microphone(self):
         speech_translation_config = speechsdk.translation.SpeechTranslationConfig(subscription=self.key, region=self.region)
-        # speech_translation_config = speechsdk.translation.SpeechTranslationConfig(subscription='790d995fc74e4aceb8fde514d8c82d6f', region='westeurope')
-
         speech_translation_config.speech_recognition_language="de-DE"
 
         target_language="en"
@@ -29,39 +27,16 @@ class Speech:
         elif translation_recognition_result.reason == speechsdk.ResultReason.NoMatch:
             # print("No speech could be recognized: {}".format(translation_recognition_result.no_match_details))
             return f"could not be recognized {translation_recognition_result.no_match_details}"
-        # elif translation_recognition_result.reason == speechsdk.ResultReason.Canceled:
-        #     cancellation_details = translation_recognition_result.cancellation_details
-        #     print("Speech Recognition canceled: {}".format(cancellation_details.reason))
-        #     if cancellation_details.reason == speechsdk.CancellationReason.Error:
-        #         print("Error details: {}".format(cancellation_details.error_details))
     
-    def translation_from_file(self, filename: str):
-        # set up translation parameters: source language and target languages
-        translation_config = speechsdk.translation.SpeechTranslationConfig(
-            subscription=self.key, region=self.region,
-            speech_recognition_language='de-DE',
-            target_languages=('en','vi'))
-        audio_config = speechsdk.audio.AudioConfig(filename=filename)
+    def speech_recognize_once_from_mic(self):
+        speech_config = speechsdk.SpeechConfig(subscription=self.key, region=self.region)
+        speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config)
 
-        # Creates a translation recognizer using and audio file as input.
-        recognizer = speechsdk.translation.TranslationRecognizer(
-            translation_config=translation_config, audio_config=audio_config)
-
-        # Starts translation, and returns after a single utterance is recognized. The end of a
-        # single utterance is determined by listening for silence at the end or until a maximum of 15
-        # seconds of audio is processed. The task returns the recognition text as result.
-        # Note: Since recognize_once() returns only a single utterance, it is suitable only for single
-        # shot recognition like command or query.
-        # For long-running multi-utterance recognition, use start_continuous_recognition() instead.
-        result = recognizer.recognize_once()
+        result = speech_recognizer.recognize_once()
 
         # Check the result
-        if result.reason == speechsdk.ResultReason.TranslatedSpeech:
-            print("""Recognized: {}
-            English: {}
-            Vietnamese: {}""".format(
-                result.text, result.translations['en'], result.translations['vi']))
-        elif result.reason == speechsdk.ResultReason.RecognizedSpeech:
-            print("Recognized: {}".format(result.text))
+        if result.reason == speechsdk.ResultReason.RecognizedSpeech:
+            return "{}".format(result.text)
         elif result.reason == speechsdk.ResultReason.NoMatch:
-            print("No speech could be recognized: {}".format(result.no_match_details))
+            return "No speech could be recognized"
+    
